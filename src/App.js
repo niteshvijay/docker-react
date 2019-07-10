d83f76b97374559868ce887bbc8c85df36649343
Nitesh Vijay <niteshvijay@gmail.com>
2019-07-10 01:45:21 -0700

    chnaged app text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          bye supesssr there..!!
+          Hi there its changed on feature branch..!!
         </p>
       </div>
     );
